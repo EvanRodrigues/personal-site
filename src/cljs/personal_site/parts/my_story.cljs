3f1d94d0b48512b68f01f068f41c126a42c3d426
EvanRodrigues <evanthomasrodrigues@gmail.com>
2019-04-26 15:30:39 -0400

    Updated color scheme and backgrounds

--- src/cljs/personal_site/parts/my_story.cljs
+++ src/cljs/personal_site/parts/my_story.cljs
@@ -11,6 +11,6 @@
 
     [:span {:class "storyEntry"} "After college, I spent a few years chasing a dream of becoming a " [:a {:class "highlighted" :href "https://twitch.tv"} "Twitch"] " streamer. In short, it wasn't as fun or rewarding as I thought it would be. While streaming, I continued to work on my programming skills. I still enjoy developing apps for the Twitch platform in my free time."]
 
-    [:span {:class "storyEntry"} "My first job was with " [:a {:class "highlighted" :href "https://bluemoonanalytics.com"} "Blue Moon Analytics"] " as a Front End Developer Consultant. Most of my work was converting their website to a serverless SPA using Clojure and re-frame. Aside from that, I constructed their blog to work without a back end, and built a pricing tool to calculate subscription costs for potential customers."]
+    [:span {:class "storyEntry"} "My first job was with " [:a {:class "highlighted" :href "https://www.bluemoonanalytics.com"} "Blue Moon Analytics"] " as a Front End Developer Consultant. Most of my work was converting their website to a serverless SPA using Clojure and re-frame. Aside from that, I constructed their blog to work without a back end, and built a pricing tool to calculate subscription costs for potential customers."]
 
     [:span {:class "storyEntry"} "As you read above, I am currently looking for Front End Developer work either remotely or in the New York City area."]]])
