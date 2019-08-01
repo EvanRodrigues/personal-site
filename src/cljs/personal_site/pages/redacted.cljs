e881be6a6c76ede3a71b41623defac79c76cd6c1
EvanRodrigues <evanthomasrodrigues@gmail.com>
2019-08-01 18:04:31 -0400

    Updated redacted project info.

--- src/cljs/personal_site/pages/redacted.cljs
+++ src/cljs/personal_site/pages/redacted.cljs
@@ -5,7 +5,7 @@
   (project-page/template
    "Redacted.tv"
    "https://www.youtube.com/embed/Akiv2Q_c8bY"
-   ["PHP" "HTML" "CSS" "WordPress"]
+   ["PHP" "HTML" "CSS" "JavaScript" "WordPress"]
    ["0:00 - Intro" "1:09 - Technologies Used" "1:48 - Twitch.tv Plugin" "3:14 - Twitter Plugin" "5:41 - Other Styling + New Sidebar" "6:35 - Article Page"]
    nil
    nil
