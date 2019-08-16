67a4bbcb1245a3f989787039651664ad0dbcedbc
EvanRodrigues <evanthomasrodrigues@gmail.com>
2019-08-16 17:10:58 -0400

    Small change to My Story section.

--- src/cljs/personal_site/parts/my_story.cljs
+++ src/cljs/personal_site/parts/my_story.cljs
@@ -9,7 +9,7 @@
     [:div {:class "storyContainer"}
      [:span {:class "storyEntry"} "I went to college at " [:a {:class "highlighted" :href "https://www.rutgers.edu"} "Rutgers University"] " in New Brunswick New Jersey, where I received my bachelor's degree in Computer Science. I chose Computer Science as my major because of my love for video games and technology."]
 
-     [:span {:class "storyEntry"} "After college, I spent three years as a " [:a {:class "highlighted" :href "https://twitch.tv"} "Twitch"] " streamer. I no longer stream on Twitch, but I develop apps for streamers as freelance work in my free time."]
+     [:span {:class "storyEntry"} "After college, I spent three years as a " [:a {:class "highlighted" :href "https://twitch.tv"} "Twitch"] " streamer. I no longer stream full-time on Twitch, but I develop apps for streamers as freelance work in my free time."]
 
      [:span {:class "storyEntry"} "My first job was with " [:a {:class "highlighted" :href "https://www.bluemoonanalytics.com"} "Blue Moon Analytics"] " as a Front End Developer Consultant. I converted their website into a serverless SPA using Clojure and re-frame. I also built their blog to work without a back-end, and developed a pricing calculator for subscription costs."]
 
